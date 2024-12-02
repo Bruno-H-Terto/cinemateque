7a0d6552939937ba4ab6b6468d6dc29cd7d3cccc
Bruno-H-Terto <herculanoterto@gmail.com>
2024-12-02 14:48:37 -0300

    Adição de capacidade para endpoint de detalhes de um cinema

--- app/controllers/api/v1/movie_theaters_controller.rb
+++ app/controllers/api/v1/movie_theaters_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MovieTheatersController < Api::V1::ApiController
   def show
     movie_theater = MovieTheater.find(params[:id])
+    max_capacity = 0
+    movie_theater.rooms.each do |room|
+      max_capacity += room.capacity
+    end
     response = movie_theater.as_json(except: [:created_at, :updated_at])
-    render status: 200, json: { movie_theater: response }
+    render status: 200, json: { movie_theater: response, max_capacity: max_capacity }
   end
 end
--- spec/requests/apis/user_view_details_a_movie_theater_spec.rb
+++ spec/requests/apis/user_view_details_a_movie_theater_spec.rb
@@ -4,6 +4,8 @@ describe 'Usuário vê detalhes de um cinema', type: :request do
   context 'GET api/v1/movie_theaters/:id' do
     it 'com sucesso' do
       movie_theater = MovieTheater.create!(name: 'Cine Santa Cruz', city: 'Juiz de Fora/MG')
+      movie_theater.rooms.create(name: 'Sala 01', capacity: 100)
+      movie_theater.rooms.create(name: 'Sala 02', capacity: 130)
 
       get "/api/v1/movie_theaters/#{movie_theater.id}"
 
@@ -12,6 +14,7 @@ describe 'Usuário vê detalhes de um cinema', type: :request do
       json_response = JSON.parse(response.body)
       expect(json_response['movie_theater']['name']).to eq 'Cine Santa Cruz'
       expect(json_response['movie_theater']['city']).to eq 'Juiz de Fora/MG'
+      expect(json_response['max_capacity']).to eq 230
       expect(json_response.inspect).not_to include 'created_at'
       expect(json_response.inspect).not_to include 'updated_at'
     end
