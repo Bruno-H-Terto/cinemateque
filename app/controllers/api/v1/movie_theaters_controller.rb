8d6107ebb16640c61c8156dd1af50ede0b2952e0
Bruno-H-Terto <herculanoterto@gmail.com>
2024-12-02 14:43:11 -0300

    Conclusão de endpoint para listagem de detalhes de um cinema

--- app/controllers/api/v1/movie_theaters_controller.rb
+++ app/controllers/api/v1/movie_theaters_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::MovieTheatersController < Api::V1::ApiController
   def show
     movie_theater = MovieTheater.find(params[:id])
-    render status: 200, json: movie_theater
+    response = movie_theater.as_json(except: [:created_at, :updated_at])
+    render status: 200, json: { movie_theater: response }
   end
 end
